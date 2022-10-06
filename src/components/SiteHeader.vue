4eaea6cc16641b5245e0eba2a9500a72e9bd9218
Leowenex <antoinecraipeau@outlook.fr>
2022-10-06 16:41:20 -0400

    Updates people

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <div>
+        <h1>WORD PANIC</h1>
+    </div>
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {{isConnected? userName : "Connexion"}}
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Login</a></li>
+            <li><a class="dropdown-item" href="#">Register</a></li>
+        </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'SiteHeader',
+    data(){
+        return {
+            isConnected: false,
+            userName: "proplayer"
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+h1{
+    color: #DCDCDD;
+}
+
+header{
+    padding: 1vw;
+    display: flex;
+    justify-content: space-between;
+    background-color: #46494C;
+    height: 10vh;
+    border-bottom: 2px solid #1985A1;
+}
+
+</style>
---
+++ src/views/GameView.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <SiteHeader/>
+        <section id="gaugeWrapper">
+            <p>{{badletters}}</p>
+        </section>
+        <section id="wordWrapper">
+            <p>{{discovered}}</p>
+        </section>
+        <section id="submitWrapper">
+            <input v-model="letter" maxlength="1" type="text" placeholder="..."/>
+            <button @click="handleSubmitLetter()">SUBMIT</button>
+        </section>
+    </div>
+</template>
+
+<script>
+
+import SiteHeader from "../components/SiteHeader.vue";
+export default {
+    name: "GameView",
+    components: { SiteHeader },
+    data(){
+        return{
+            letter:"",
+            word:"tuna",
+            discovered:[],
+            badletters:[]
+        }
+    },
+    beforeMount(){
+        for(let i= 0;i<this.word.length;i++){
+            this.discovered[i] = "#"
+        }
+        fetch('/api/users',{method: 'GET',})
+        .then((response)=>{console.log(response.json())})
+    },
+    beforeUnmount(){
+        alert("You will lose your progression if you leave the game");
+    },
+    methods:{
+        handleSubmitLetter(){
+            if(this.letter!==''){
+                if(this.badletters.includes(this.letter) || this.discovered.includes(this.letter)){
+                    alert("The letter has already been entered")
+                }else{
+                    if(this.word.indexOf(this.letter)>(-1)){
+                        this.discovered[this.word.indexOf(this.letter)] = this.letter;
+                        if(!this.discovered.includes("#")){
+                            this.handleVictory();
+                        }
+                    }else{
+                        this.badletters.push(this.letter);
+                    } 
+                }
+                this.letter="";
+            }
+        },
+        handleVictory(){
+            alert("Victory");
+            this.$router.replace("/")
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
